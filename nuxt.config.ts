47846d92a6c0cfcaa4d3ad37f29473eed86545cc
Jeffri Andriyanto <jeff.tm07@gmail.com>
2025-08-26 14:43:32 +0700

    Add SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,14 @@ export default defineNuxtConfig({
     "nuxt-schema-org",
   ],
 
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "id",
+      },
+    },
+  },
+
   css: ["~/assets/css/main.css"],
 
   aos: {
